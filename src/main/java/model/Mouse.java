26cdd2744862438fe7042b77d314ad71442720df
Pavel Kazachenko <pkazachenkoo@gmail.com>
2024-08-07 03:55:27 +0300

    Changed squeak logic

--- src/main/java/model/Mouse.java
+++ src/main/java/model/Mouse.java
@@ -3,9 +3,10 @@ package model;
 public class Mouse {
     public Mouse() {
         System.out.println("I'm a mouse!");
+        squeak();
     }
 
-    public void squeak() {
+    private void squeak() {
         System.out.println("Squeak!");
     }
 }
