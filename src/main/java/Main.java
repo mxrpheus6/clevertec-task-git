afeb03b9cc285951ec38f5c27e99f1649f7c1de9
Pavel Kazachenko <pkazachenkoo@gmail.com>
2024-08-07 03:24:30 +0300

    Changed main logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
 import model.Man;
+import model.service.ManService;
 
 public class Main {
     public static void main(String[] args) {
         Man man = new Man();
+        ManService manService = new ManService();
+        manService.work(man);
     }
 }
